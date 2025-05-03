cc12421f3e5f146b1acc07350397e338055da746
Allison Daney <daney.allison69@gmail.com>
2025-05-03 22:44:28 +0200

    feat(layouts): add default layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col min-h-screen overflow-hidden">
+    <AppHeader />
+
+    <main class="flex-1 flex items-center justify-center">
+      <slot />
+    </main>
+
+    <AppFooter />
+  </div>
+</template>
