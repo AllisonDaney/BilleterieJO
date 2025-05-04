e48622978e88eedb341cb3b427377d4fb31c7ef1
Allison Daney <daney.allison69@gmail.com>
2025-05-04 16:26:08 +0200

    feat(signin): add signin page

---
+++ app/layouts/default-blank.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex flex-col min-h-screen overflow-hidden">
+    <main class="flex-1 flex items-center justify-center">
+      <slot />
+    </main>
+  </div>
+</template>
