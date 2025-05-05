2ae99de3fffb7acb9ba61a2b7f67245713cfea79
Allison Daney <daney.allison69@gmail.com>
2025-05-05 13:05:15 +0200

    feat(database): add drizzle helper, add seeder, schema and migration

---
+++ server/utils/useDrizzle.ts
@@ -0,0 +1,52 @@
+import { PgTable } from 'drizzle-orm/pg-core'
+import type { PostgresJsDatabase } from 'drizzle-orm/postgres-js'
+import { sql } from 'drizzle-orm'
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
+import 'dotenv/config'
+
+export function useDrizzle() {
+  const url = process.env.NUXT_POSTGRES_URL
+
+  if (!url) {
+    throw createError({ statusCode: 500, statusMessage: 'Missing NUXT_POSTGRES_URL' })
+  }
+
+  const client = postgres(url, { ssl: 'require' })
+  const db = drizzle(client)
+
+  return { db, client }
+}
+
+export function useDrizzleToSeed() {
+  const url = process.env.NUXT_POSTGRES_URL
+
+  if (!url) {
+    throw new Error('Missing NUXT_POSTGRES_URL')
+  }
+
+  const client = postgres(url, { ssl: 'require' })
+  const db = drizzle(client)
+
+  return { db, client }
+}
+
+export async function reset(
+  db: PostgresJsDatabase<Record<string, never>>,
+  ...tables: PgTable[]
+) {
+  // @ts-ignore
+  const tableNames = tables.map((table) => table[PgTable.Symbol.Name])
+  
+  if (tableNames.length === 0)
+    return
+
+  const joined = sql.join(
+    tableNames.map(name => sql.identifier(name)),
+    sql`, `,
+  )
+
+  await db.execute(
+    sql`TRUNCATE ${joined} RESTART IDENTITY CASCADE`,
+  )
+}
