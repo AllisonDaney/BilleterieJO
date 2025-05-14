272f9d1dd37a02216e78ae1d7118e1dcd061a70f
Allison Daney <daney.allison69@gmail.com>
2025-05-14 08:09:57 +0200

    feat(schema): add users_tickets and tickets to schema

---
+++ server/database/schema/users_tickets.ts
@@ -0,0 +1,23 @@
+import { relations, sql } from 'drizzle-orm'
+import { boolean, pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core'
+import { tickets } from './tickets'
+import { users } from './users'
+
+export const usersTickets = pgTable('users_tickets', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  userId: uuid('user_id').notNull(),
+  ticketId: uuid('ticket_id').notNull(),
+  isUsed: boolean('is_used').default(false),
+  qrCodeUrl: text('qr_code_url').notNull(),
+  securityKey: text('security_key').notNull(),
+  timestamp: timestamp('timestamp').notNull(),
+  createdAt: timestamp('created_at').defaultNow(),
+})
+
+export const usersRelations = relations(users, ({ many }) => ({
+  tickets: many(usersTickets),
+}))
+
+export const ticketsRelations = relations(tickets, ({ many }) => ({
+  users: many(usersTickets),
+}))
