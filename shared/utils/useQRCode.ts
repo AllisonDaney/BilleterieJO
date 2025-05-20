9668cc778c3b7a10caa8ecad05cd5fa0d6cf568b
Allison Daney <daney.allison69@gmail.com>
2025-05-20 23:17:55 +0200

    fix(qrcode): qrcode import

--- shared/utils/useQRCode.ts
+++ shared/utils/useQRCode.ts
@@ -1,8 +1,11 @@
-import QRCode from 'qrcode'
+import QRCode from 'qr-image'
 
 export function useQRCode() {
   async function generateQRCodeWithUrl(url: string) {
-    return await QRCode.toDataURL(url)
+    const buffer = QRCode.imageSync(url, { type: 'png', size: 10, margin: 1 })
+    const base64 = buffer.toString('base64')
+    
+    return `data:image/png;base64,${base64}`
   }
 
   return {
